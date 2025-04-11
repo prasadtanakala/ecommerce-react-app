b7bb0e50944ebdddec8c2f5c0c134f2b1907072b
prasadtanakala <prasadtanakala76@gmail.com>
2025-04-11 14:56:47 +0530

    first-commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,26 @@
 import React from 'react'
 import { Navigate } from 'react-router-dom'
+import LoginComponent from './components/LoginComponent/LoginComponent'
+import HomeComponent from './components/HomeComponent/HomeComponent'
+import ProductDetailsComponent from './components/ProductDetailsComponent/ProductDetailsComponent'
+import CartComponent from './components/CartComponent/CartComponent'
+import HeaderComponent from './components/HeaderComponent/HeaderComponent'
 
 
 const App = () => {
   const PrivateRoute = ({ children }) => {
-    return localStorage.getItem('token') ? <> <Header /> {children} </> : <Navigate to="/" />
+    return localStorage.getItem('token') ? <> <HeaderComponent /> {children} </> : <Navigate to="/" />
   }
   return (
     <>
-      Hello World
+         <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LoginComponent />} />
+        <Route path="/home" element={<PrivateRoute><HomeComponent /></PrivateRoute>} />
+        <Route path="/product/:id" element={<PrivateRoute><ProductDetailsComponent /></PrivateRoute>} />
+        <Route path="/cart" element={<PrivateRoute><CartComponent /></PrivateRoute>} />
+      </Routes>
+      </BrowserRouter>
     </>
   )
 }
---
+++ src/components/HeaderComponent/HeaderComponent.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import './HeaderComponent.css'
+import { useNavigate } from 'react-router-dom';
+const HeaderComponent = () => {
+    const navigate =useNavigate();
+    const handleLogout = () => {
+      localStorage.removeItem('token');
+      navigate('/'); 
+    };
+  return (
+    <>
+    <header className="ecommerce-header">
+     <div className="header-container">
+       <Link to="/home" className="logo">
+         Shopping
+       </Link>
+       
+       <nav className="nav-links">
+         <Link to="/home" className="nav-link">
+           <FaHome className="nav-icon" />
+           <span>Home</span>
+         </Link>
+         
+         <Link to="/cart" className="nav-link cart-link">
+           <FaShoppingCart className="nav-icon" />
+           <span>cart</span>
+         </Link>
+         
+         <button  onClick={handleLogout} className="nav-link logout-btn">
+           <FaSignOutAlt  className="nav-icon" />
+           <span>Logout</span>
+         </button>
+       </nav>
+     </div>
+   </header>
+  </>
+  )
+}
+
+export default HeaderComponent
