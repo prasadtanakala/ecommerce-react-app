10269f7aabd16109489ed1008fb55c926a23a906
prasadtanakala <prasadtanakala76@gmail.com>
2025-04-11 14:18:32 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,6 @@
 import React from 'react'
-import { Routes, Route, Navigate, BrowserRouter } from 'react-router-dom'
-import Home from './components/home/Home'
-import Login from './components/login/Login'
-import Product from './components/product/Product'
-import Cart from './components/cart/Cart'
-import Header from './components/header/Header'
-import Productpiechart from './components/productPieChart/Productpiechart'
+import { Navigate } from 'react-router-dom'
+
 
 const App = () => {
   const PrivateRoute = ({ children }) => {
@@ -13,15 +8,7 @@ const App = () => {
   }
   return (
     <>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Login/>} />
-        <Route path="/home" element={<PrivateRoute><Home/></PrivateRoute>} />
-        <Route path="/product/:id" element={<PrivateRoute><Product/></PrivateRoute>} />
-        <Route path="/cart" element={<PrivateRoute><Cart/></PrivateRoute>} />
-        <Route path="/pichart" element={<PrivateRoute><Productpiechart/></PrivateRoute>} />
-      </Routes>
-      </BrowserRouter>
+      Hello World
     </>
   )
 }
